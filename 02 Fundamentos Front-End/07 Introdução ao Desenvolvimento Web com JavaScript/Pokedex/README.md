36683cb94def776b0c779b1c167371d949b176cc
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-01-21 15:15:55 -0300

    Finalizando a Pokedex

---
+++ 02 Fundamentos Front-End/07 Introdução ao Desenvolvimento Web com JavaScript/Pokedex/README.md
@@ -0,0 +1,4 @@
+# Importante!
+Não dei continuidade a Pokedex oferecida pelo Bootcamp, pois tudo estava extremamente lento e, no meu ponto de vista, mal planejado.
+
+Portanto, minha Pokedex será feita separadamente e de minha maneira.
--- 02 Fundamentos Front-End/07 Introdução ao Desenvolvimento Web com JavaScript/Pokedex/src/js/main.js
+++ 02 Fundamentos Front-End/07 Introdução ao Desenvolvimento Web com JavaScript/Pokedex/src/js/main.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("ola");
+
+// const OFFSET = 0;
+// const LIMIT = 151;
+let pokemonId = 1;
+const POKEAPI_URL = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+
+fetch(POKEAPI_URL) // Por padrão, usa o GET
+     .then((response) => response.json())
+     .then((response) => console.log(response)) // Recebe o return do then anterior
+     .catch((error) => console.log(error));
